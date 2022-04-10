34fd57224f018b3dfff200f3b7e370570f03fa7a
supernovaw <ruszrj@gmail.com>
2022-04-10 10:36:34 -0700

    initial commit

---
+++ counters.js
@@ -0,0 +1,640 @@
+(() => {
+	const counterGroups = [
+		{
+			"name": "hon",
+			"word": "本",
+			"furi": "ほん",
+			"howMany": "なんぼん",
+			"conjunctions": [
+				"じゅっぽん",
+				"いっぽん",
+				"にほん",
+				"さんぼん",
+				"よんほん",
+				"ごほん",
+				"ろっぽん",
+				"ななほん",
+				"はっぽん",
+				"きゅうほん"
+			],
+			"examples": [
+				{
+					"word": "ペン",
+					"furi": "ペン",
+					"translation_en": [
+						"pen",
+						"pens"
+					],
+					"image": "hon/pen.jpeg"
+				},
+				{
+					"word": "木",
+					"furi": "き",
+					"translation_en": [
+						"tree",
+						"trees"
+					],
+					"image": "hon/tree.jpeg"
+				},
+				{
+					"word": "電車",
+					"furi": "でんしゃ",
+					"translation_en": [
+						"train",
+						"trains"
+					],
+					"image": "hon/train.jpeg"
+				},
+				{
+					"word": "川",
+					"furi": "かわ",
+					"translation_en": [
+						"river",
+						"rivers"
+					],
+					"image": "hon/river.jpeg"
+				},
+				{
+					"word": "道路",
+					"furi": "どうろ",
+					"translation_en": [
+						"road",
+						"roads"
+					],
+					"image": "hon/road.jpg"
+				}
+			]
+		},
+		{
+			"name": "ko",
+			"word": "個",
+			"furi": "こ",
+			"howMany": "なんこ",
+			"conjunctions": [
+				"じゅっこ",
+				"いっこ",
+				"にこ",
+				"さんこ",
+				"よんこ",
+				"ごこ",
+				"ろっこ",
+				"ななこ",
+				"はっこ",
+				"きゅうこ"
+			],
+			"examples": [
+				{
+					"word": "帽子",
+					"furi": "ぼうし",
+					"translation_en": [
+						"hat",
+						"hats"
+					],
+					"image": "ko/hat.jpeg"
+				},
+				{
+					"word": "みずたまり",
+					"furi": "みずたまり",
+					"translation_en": [
+						"puddle",
+						"puddles"
+					],
+					"image": "ko/puddle.jpeg"
+				},
+				{
+					"word": "ファイル",
+					"furi": "ファイル",
+					"translation_en": [
+						"file",
+						"files"
+					],
+					"image": "ko/file.jpg"
+				},
+				{
+					"word": "リンゴ",
+					"furi": "リンゴ",
+					"translation_en": [
+						"apple",
+						"apples"
+					],
+					"image": "ko/apple.jpeg"
+				},
+				{
+					"word": "石",
+					"furi": "いし",
+					"translation_en": [
+						"stone",
+						"stones"
+					],
+					"image": "ko/stone.jpeg"
+				}
+			]
+		},
+		{
+			"name": "hiki",
+			"word": "匹",
+			"furi": "ひき",
+			"howMany": "なんびき",
+			"conjunctions": [
+				"じゅっぴき",
+				"いっぴき",
+				"にひき",
+				"さんびき",
+				"よんひき",
+				"ごひき",
+				"ろっぴき",
+				"ななひき",
+				"はっぴき",
+				"きゅうひき"
+			],
+			"examples": [
+				{
+					"word": "犬",
+					"furi": "いぬ",
+					"translation_en": [
+						"dog",
+						"dogs"
+					],
+					"image": "hiki/dog.jpeg"
+				},
+				{
+					"word": "猫",
+					"furi": "ねこ",
+					"translation_en": [
+						"cat",
+						"cats"
+					],
+					"image": "hiki/cat.jpeg"
+				},
+				{
+					"word": "魚",
+					"furi": "うお",
+					"translation_en": [
+						"fish",
+						"fish"
+					],
+					"image": "hiki/fish.jpeg"
+				},
+				{
+					"word": "鹿",
+					"furi": "しか",
+					"translation_en": [
+						"deer",
+						"deer"
+					],
+					"image": "hiki/deer.jpeg"
+				},
+				{
+					"word": "ヘビ",
+					"furi": "ヘビ",
+					"translation_en": [
+						"snake",
+						"snakes"
+					],
+					"image": "hiki/snake.jpeg"
+				}
+			]
+		},
+		{
+			"name": "tou",
+			"word": "頭",
+			"furi": "とう",
+			"howMany": "なんとう",
+			"conjunctions": [
+				"じゅっとう",
+				"いっとう",
+				"にとう",
+				"さんとう",
+				"よんとう",
+				"ごとう",
+				"ろくとう",
+				"ななとう",
+				"はっとう",
+				"きゅうとう"
+			],
+			"examples": [
+				{
+					"word": "牛",
+					"furi": "うし",
+					"translation_en": [
+						"cow",
+						"cows"
+					],
+					"image": "tou/cow.jpeg"
+				},
+				{
+					"word": "馬",
+					"furi": "うま",
+					"translation_en": [
+						"horse",
+						"horses"
+					],
+					"image": "tou/horse.jpeg"
+				},
+				{
+					"word": "象",
+					"furi": "ぞう",
+					"translation_en": [
+						"elephant",
+						"elephants"
+					],
+					"image": "tou/elephant.jpeg"
+				},
+				{
+					"word": "クジラ",
+					"furi": "クジラ",
+					"translation_en": [
+						"whale",
+						"whales"
+					],
+					"image": "tou/whale.jpeg"
+				},
+				{
+					"word": "トラ",
+					"furi": "トラ",
+					"translation_en": [
+						"tiger",
+						"tigers"
+					],
+					"image": "tou/tiger.jpeg"
+				}
+			]
+		},
+		{
+			"name": "satsu",
+			"word": "冊",
+			"furi": "さつ",
+			"howMany": "なんさつ",
+			"conjunctions": [
+				"じゅっさつ",
+				"いっさつ",
+				"にさつ",
+				"さんさつ",
+				"よんさつ",
+				"ごさつ",
+				"ろくさつ",
+				"ななさつ",
+				"はっさつ",
+				"きゅうさつ"
+			],
+			"examples": [
+				{
+					"word": "本",
+					"furi": "ほん",
+					"translation_en": [
+						"book",
+						"books"
+					],
+					"image": "satsu/book.jpeg"
+				},
+				{
+					"word": "ノートブック",
+					"furi": "ノートブック",
+					"translation_en": [
+						"note book",
+						"note books"
+					],
+					"image": "satsu/note book.jpeg"
+				},
+				{
+					"word": "ドキュメント",
+					"furi": "ドキュメント",
+					"translation_en": [
+						"document",
+						"documents"
+					],
+					"image": "satsu/document.jpeg"
+				},
+				{
+					"word": "辞書",
+					"furi": "じしょ",
+					"translation_en": [
+						"dictionary",
+						"dictionaries"
+					],
+					"image": "satsu/dictionary.jpg"
+				},
+				{
+					"word": "小冊子",
+					"furi": "しょうさっし",
+					"translation_en": [
+						"booklet",
+						"booklets"
+					],
+					"image": "satsu/booklet.png"
+				}
+			]
+		},
+		{
+			"name": "mai",
+			"word": "枚",
+			"furi": "まい",
+			"howMany": "なんまい",
+			"conjunctions": "default",
+			"examples": [
+				{
+					"word": "紙",
+					"furi": "かみ",
+					"translation_en": [
+						"paper sheet",
+						"paper sheets"
+					],
+					"image": "mai/paper_sheet.jpeg"
+				},
+				{
+					"word": "写真",
+					"furi": "しゃしん",
+					"translation_en": [
+						"photograph",
+						"photographs"
+					],
+					"image": "mai/photograph.jpeg"
+				},
+				{
+					"word": "カード",
+					"furi": "カード",
+					"translation_en": [
+						"bank card",
+						"bank cards"
+					],
+					"image": "mai/bank_card.jpeg"
+				},
+				{
+					"word": "ワイシャツ",
+					"furi": "ワイシャツ",
+					"translation_en": [
+						"dress shirt",
+						"dress shirts"
+					],
+					"image": "mai/dress_shirt.jpeg"
+				},
+				{
+					"word": "スカート",
+					"furi": "スカート",
+					"translation_en": [
+						"skirt",
+						"skirts"
+					],
+					"image": "mai/skirt.jpg"
+				}
+			]
+		},
+		{
+			"name": "dai",
+			"word": "台",
+			"furi": "だい",
+			"howMany": "なんだい",
+			"conjunctions": "default",
+			"examples": [
+				{
+					"word": "ベッド",
+					"furi": "ベッド",
+					"translation_en": [
+						"bed",
+						"beds"
+					],
+					"image": "dai/bed.jpeg"
+				},
+				{
+					"word": "テーブル",
+					"furi": "テーブル",
+					"translation_en": [
+						"table",
+						"tables"
+					],
+					"image": "dai/table.jpeg"
+				},
+				{
+					"word": "車",
+					"furi": "くるま",
+					"translation_en": [
+						"car",
+						"cars"
+					],
+					"image": "dai/car.jpg"
+				},
+				{
+					"word": "電子レンジ",
+					"furi": "でんしれんじ",
+					"translation_en": [
+						"microwave",
+						"microwaves"
+					],
+					"image": "dai/microwave.jpeg"
+				},
+				{
+					"word": "電話",
+					"furi": "でんわ",
+					"translation_en": [
+						"phone",
+						"phones"
+					],
+					"image": "dai/phone.jpeg"
+				}
+			]
+		}
+	];
+	const selfCounters = [
+		{
+			"name": "second",
+			"word": "秒",
+			"furi": "びょう",
+			"howMany": "なんびょう",
+			"translation_en": [
+				"second",
+				"seconds"
+			],
+			"conjunctions": "default"
+		},
+		{
+			"name": "minute",
+			"word": "分",
+			"furi": "ぶん",
+			"howMany": "なんぷん",
+			"translation_en": [
+				"minute",
+				"minutes"
+			],
+			"conjunctions": [
+				"じゅっぷん",
+				"いっぷん",
+				"にふん",
+				"さんぷん",
+				"よんぷん",
+				"ごふん",
+				"ろっぷん",
+				"ななふん",
+				"はっぷん",
+				"きゅうふん"
+			]
+		},
+		{
+			"name": "hour",
+			"word": "時",
+			"furi": "じ",
+			"howMany": "なんじ",
+			"translation_en": [
+				"hour",
+				"hours"
+			],
+			"conjunctions": [
+				"じゅうじ",
+				"いちじ",
+				"にじ",
+				"さんじ",
+				"よじ",
+				"ごじ",
+				"ろくじ",
+				"しちじ",
+				"はちじ",
+				"くじ"
+			]
+		},
+		{
+			"name": "day_interval",
+			"word": "日",
+			"furi": "にち",
+			"howMany": "なんにち",
+			"translation_en": [
+				"day",
+				"days"
+			],
+			"conjunctions": [
+				"じゅうにち",
+				"いちにち",
+				"ににち",
+				"さんにち",
+				"よっか",
+				"ごにち",
+				"ろくにち",
+				"ななにち",
+				"はちにち",
+				"くにち"
+			],
+			"exceptions":
+				[{ "number": 3, "reading": "みっか" },
+				{ "number": 4, "reading": "よっか" },
+				{ "number": 5, "reading": "いつか" },
+				{ "number": 6, "reading": "むいか" },
+				{ "number": 7, "reading": "なのか" },
+				{ "number": 8, "reading": "ようか" },
+				{ "number": 9, "reading": "ここのか" },
+				{ "number": 10, "reading": "とおか" },
+				{ "number": 20, "reading": "はつか" }]
+		},
+		{
+			"name": "week",
+			"word": "週",
+			"furi": "しゅう",
+			"howMany": "なんしゅう",
+			"translation_en": [
+				"week",
+				"weeks"
+			],
+			"conjunctions": [
+				"じゅっしゅう",
+				"いっしゅう",
+				"にしゅう",
+				"さんしゅう",
+				"よんしゅう",
+				"ごしゅう",
+				"ろくしゅう",
+				"ななしゅう",
+				"はっしゅう",
+				"きゅうしゅう"
+			]
+		},
+		{
+			"name": "month_interval",
+			"word": "ヶ月",
+			"furi": "かげつ",
+			"howMany": "なんかげつ",
+			"translation_en": [
+				"month",
+				"months"
+			],
+			"conjunctions": [
+				"じゅっかげつ",
+				"いっかげつ",
+				"にかげつ",
+				"さんかげつ",
+				"よんかげつ",
+				"ごかげつ",
+				"ろっかげつ",
+				"ななかげつ",
+				"はっかげつ",
+				"きゅうかげつ"
+			]
+		},
+		{
+			"name": "year",
+			"word": "年",
+			"furi": "ねん",
+			"howMany": "なんねん",
+			"translation_en": [
+				"year",
+				"years"
+			],
+			"conjunctions": [
+				"じゅうねん",
+				"いちねん",
+				"にねん",
+				"さんねん",
+				"よねん",
+				"ごねん",
+				"ろくねん",
+				"ななねん",
+				"はちねん",
+				"きゅうねん"
+			]
+		},
+		{
+			"name": "kai",
+			"word": "回",
+			"furi": "かい",
+			"howMany": "なんかい",
+			"translation_en": [
+				"time",
+				"times"
+			],
+			"conjunctions": [
+				"じゅっかい",
+				"いっかい",
+				"にかい",
+				"さんかい",
+				"よんかい",
+				"ごかい",
+				"ろっかい",
+				"ななかい",
+				"はっかい",
+				"きゅうかい"
+			]
+		},
+		{
+			"name": "nin",
+			"word": "人",
+			"furi": "にん",
+			"howMany": "なんにん",
+			"translation_en": [
+				"person",
+				"people"
+			],
+			"conjunctions": [
+				"じゅうにん",
+				"いちにん",
+				"ににん",
+				"さんにん",
+				"よにん",
+				"ごにん",
+				"ろくにん",
+				"ななにん",
+				"はちにん",
+				"きゅうにん"
+			],
+			"exceptions":
+				[{ "number": 1, "reading": "ひとり" },
+				{ "number": 2, "reading": "ふたり" }]
+		}
+	];
+	counters = {"counterGroups": counterGroups, "selfCounters": selfCounters};
+})();
---
+++ script.js
@@ -0,0 +1,161 @@
+'use strict';
+(() => {
+	const kanjiNumber = (number) => {
+		const pos0 = number % 10;
+		const pos1 = Math.floor(number / 10);
+		const kanji = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+		const kanjiTen = "十";
+		if (pos1 == 0) return kanji[pos0];
+		if (pos1 == 1) return kanjiTen + kanji[pos0];
+		return kanji[pos1] + kanjiTen + kanji[pos0];
+	}
+
+	const kanaNumber = (counter, number) => {
+		if (counter.exceptions) {
+			const exc = counter.exceptions.find(e => e.number === number);
+			if (exc) return exc.reading;
+		}
+		const pos0 = number % 10;
+		const pos1 = Math.floor(number / 10);
+		const kana = ["", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"];
+		const kanaTen = "じゅう";
+		let conj = counter.conjunctions;
+		if (conj === "default") conj = ["じゅう", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"]
+			.map(s => s + counter.furi);
+
+		if (pos1 == 0) return conj[pos0]; // 1-9
+		if (number == 10) return conj[0]; // 10
+		if (pos1 == 1) return kanaTen + conj[pos0]; // 11-19
+		if (pos0 == 0) return kana[pos1] + conj[0]; // 20, 30,.. 90
+		return kana[pos1] + kanaTen + conj[pos0]; // 21, 22,..
+	}
+
+	const randomFlashcardCount = () => {
+		const rand = Math.random();
+		if (rand < 0.36) return Math.ceil(rand / 0.36 * 10); // 1 to 10
+		if (rand < 0.63) return Math.ceil((rand - 0.36) / 0.27 * 10 + 10); // 11 to 20
+		if (rand < 0.765) return Math.floor((rand - 0.63) / 0.135 * 7 + 3) * 10; // 30, 40... to 90
+		if (rand < 0.9) return Math.floor((rand - 0.765) / 0.135 * 79 + 21); // 21, 22... to 99
+		return -1; // interrogative (e.g. nankai)
+	}
+
+	const createDateFlashcard = () => {
+		let month = Math.floor(Math.random() * 12 + 1); // 1 = Jan
+		let maxDays;
+		switch (month) {
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+				maxDays = 31; break;
+			case 4: case 6: case 9: case 11:
+				maxDays = 30; break;
+			default: maxDays = 28; break;
+		}
+		let day = Math.ceil(Math.random() * maxDays);
+		if (Math.random() < 0.00069) { // lucky enough for Feb 29?
+			month = 2;
+			day = 29;
+		}
+		let suffixEn;
+		switch (day) {
+			case 1: case 21: case 31: suffixEn = "st"; break;
+			case 2: case 22: suffixEn = "nd"; break;
+			default: suffixEn = "th"; break;
+		}
+
+		const monthNamesJa = ["いちがつ", "にがつ", "さんがつ", "しがつ", "ごがつ", "ろくがつ",
+			"しちがつ", "はちがつ", "くがつ", "じゅうがつ", "じゅういちがつ", "じゅうにがつ"];
+		const monthNamesEn = ["January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"];
+		const dayReadings = ["ついたち", "ふつか", "みっか", "よっか", "いつか", "むいか",
+			"なのか", "ようか", "ここのか", "とおか", "じゅういちにち", "じゅうににち", "じゅうさんにち",
+			"じゅうよっか", "じゅうごにち", "じゅうろくにち", "じゅうななにち", "じゅうはちにち",
+			"じゅうくにち", "はつか", "にじゅういちにち", "にじゅうににち", "にじゅうさんにち",
+			"にじゅうよっか", "にじゅうごにち", "にじゅうろくにち", "にじゅうななにち",
+			"にじゅうはちにち", "にじゅうくにち", "さんじゅうにち", "さんじゅういちにち"];
+
+		return {
+			"japanese": kanjiNumber(month) + "月" + kanjiNumber(day) + "日",
+			"furigana": monthNamesJa[month - 1] + dayReadings[day - 1],
+			"translation": day + suffixEn + " of " + monthNamesEn[month - 1],
+			"image": null
+		}
+	};
+
+	const createCounterGroupFlashcard = (counter, number) => {
+		const example = counter.examples[Math.floor(Math.random() * counter.examples.length)];
+		if (number == -1) return {
+			"japanese": "何" + counter.word + "の" + example.word,
+			"furigana": counter.howMany + "の" + example.furi,
+			"translation": "how many " + example.translation_en[1] + "?",
+			"image": "counter-images/" + example.image
+		};
+		return {
+			"japanese": kanjiNumber(number) + counter.word + "の" + example.word,
+			"furigana": kanaNumber(counter, number) + "の" + example.furi,
+			"translation": number + " " + example.translation_en[number == 1 ? 0 : 1],
+			"image": "counter-images/" + example.image
+		};
+	};
+
+	const createSelfCounterFlashcard = (counter, number) => {
+		if (number == -1) return {
+			"japanese": "何" + counter.word,
+			"furigana": counter.howMany,
+			"translation": "how many " + counter.translation_en[1] + "?",
+			"image": null
+		};
+		return {
+			"japanese": kanjiNumber(number) + counter.word,
+			"furigana": kanaNumber(counter, number),
+			"translation": number + " " + counter.translation_en[number == 1 ? 0 : 1],
+			"image": null
+		};
+	};
+
+	const createRandomFlashcard = () => {
+		// +1 counter is the special case for month & day
+		const countersTotal = counters.counterGroups.length + counters.selfCounters.length + 1;
+		let drawIndex = Math.floor(Math.random() * countersTotal) - 1;
+		if (drawIndex == -1) return createDateFlashcard();
+		const randomCount = randomFlashcardCount();
+
+		if (drawIndex < counters.counterGroups.length)
+			return createCounterGroupFlashcard(counters.counterGroups[drawIndex], randomCount);
+		drawIndex -= counters.counterGroups.length;
+		return createSelfCounterFlashcard(counters.selfCounters[drawIndex], randomCount);
+	};
+
+	const flashcardDom = {
+		flashcard: document.querySelector(".flashcard"),
+		jp: document.querySelector(".flashcard .japanese rb"),
+		jpFuri: document.querySelector(".flashcard .japanese rt"),
+		translation: document.querySelector(".flashcard .translation"),
+		image: document.querySelector(".flashcard .card-image img")
+	}
+
+	let isAnswerShown = true;
+	let currentCard;
+
+	const assignNewFlashcard = () => {
+		currentCard = createRandomFlashcard();
+		flashcardDom.flashcard.classList.add("hide-answer");
+		flashcardDom.jp.innerHTML = "　";
+		flashcardDom.jpFuri.innerHTML = "　";
+		flashcardDom.translation.innerHTML = currentCard.translation;
+		flashcardDom.image.style.visibility = currentCard.image === null ? "hidden" : "visible";
+		flashcardDom.image.src = currentCard.image === null ? "" : currentCard.image;
+	}
+
+	const showAnswer = () => {
+		flashcardDom.flashcard.classList.remove("hide-answer");
+		flashcardDom.jp.innerHTML = currentCard.japanese;
+		flashcardDom.jpFuri.innerHTML = currentCard.furigana;
+	}
+
+	const onContinueClicked = () => {
+		if (isAnswerShown) assignNewFlashcard();
+		else showAnswer();
+		isAnswerShown = !isAnswerShown;
+	};
+
+	document.querySelector(".continue-button").onclick = e => onContinueClicked();
+})();
