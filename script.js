b0a4998b48af15ba82529885aea1e7563175ca71
supernovaw <ruszrj@gmail.com>
2022-04-14 02:04:19 -0700

    pace counter added

--- script.js
+++ script.js
@@ -1,117 +1,118 @@
 'use strict';
 (() => {
-	const kanjiNumber = (number) => {
-		const pos0 = number % 10;
-		const pos1 = Math.floor(number / 10);
-		const kanji = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
-		const kanjiTen = "十";
-		if (pos1 == 0) return kanji[pos0];
-		if (pos1 == 1) return kanjiTen + kanji[pos0];
-		return kanji[pos1] + kanjiTen + kanji[pos0];
-	}
-
-	const kanaNumber = (counter, number) => {
-		if (counter.exceptions) {
-			const exc = counter.exceptions.find(e => e.number === number);
-			if (exc) return exc.reading;
-		}
-		const pos0 = number % 10;
-		const pos1 = Math.floor(number / 10);
-		const kana = ["", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"];
-		const kanaTen = "じゅう";
-		let conj = counter.conjunctions;
-		if (conj === "default") conj = ["じゅう", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"]
-			.map(s => s + counter.furi);
-
-		if (pos1 == 0) return conj[pos0]; // 1-9
-		if (number == 10) return conj[0]; // 10
-		if (pos1 == 1) return kanaTen + conj[pos0]; // 11-19
-		if (pos0 == 0) return kana[pos1] + conj[0]; // 20, 30,.. 90
-		return kana[pos1] + kanaTen + conj[pos0]; // 21, 22,..
-	}
-
-	const randomFlashcardCount = () => {
-		const rand = Math.random();
-		if (rand < 0.36) return Math.ceil(rand / 0.36 * 10); // 1 to 10
-		if (rand < 0.63) return Math.ceil((rand - 0.36) / 0.27 * 10 + 10); // 11 to 20
-		if (rand < 0.765) return Math.floor((rand - 0.63) / 0.135 * 7 + 3) * 10; // 30, 40... to 90
-		if (rand < 0.9) return Math.floor((rand - 0.765) / 0.135 * 79 + 21); // 21, 22... to 99
-		return -1; // interrogative (e.g. nankai)
-	}
-
-	const createDateFlashcard = () => {
-		let month = Math.floor(Math.random() * 12 + 1); // 1 = Jan
-		let maxDays;
-		switch (month) {
-			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-				maxDays = 31; break;
-			case 4: case 6: case 9: case 11:
-				maxDays = 30; break;
-			default: maxDays = 28; break;
-		}
-		let day = Math.ceil(Math.random() * maxDays);
-		if (Math.random() < 0.00069) { // lucky enough for Feb 29?
-			month = 2;
-			day = 29;
-		}
-		let suffixEn;
-		switch (day) {
-			case 1: case 21: case 31: suffixEn = "st"; break;
-			case 2: case 22: suffixEn = "nd"; break;
-			default: suffixEn = "th"; break;
-		}
-
-		const monthNamesJa = ["いちがつ", "にがつ", "さんがつ", "しがつ", "ごがつ", "ろくがつ",
-			"しちがつ", "はちがつ", "くがつ", "じゅうがつ", "じゅういちがつ", "じゅうにがつ"];
-		const monthNamesEn = ["January", "February", "March", "April", "May", "June",
-			"July", "August", "September", "October", "November", "December"];
-		const dayReadings = ["ついたち", "ふつか", "みっか", "よっか", "いつか", "むいか",
-			"なのか", "ようか", "ここのか", "とおか", "じゅういちにち", "じゅうににち", "じゅうさんにち",
-			"じゅうよっか", "じゅうごにち", "じゅうろくにち", "じゅうななにち", "じゅうはちにち",
-			"じゅうくにち", "はつか", "にじゅういちにち", "にじゅうににち", "にじゅうさんにち",
-			"にじゅうよっか", "にじゅうごにち", "にじゅうろくにち", "にじゅうななにち",
-			"にじゅうはちにち", "にじゅうくにち", "さんじゅうにち", "さんじゅういちにち"];
-
-		return {
-			"japanese": kanjiNumber(month) + "月" + kanjiNumber(day) + "日",
-			"furigana": monthNamesJa[month - 1] + dayReadings[day - 1],
-			"translation": day + suffixEn + " of " + monthNamesEn[month - 1],
-			"image": null
-		}
-	};
-
-	const createCounterGroupFlashcard = (counter, number) => {
-		const example = counter.examples[Math.floor(Math.random() * counter.examples.length)];
-		if (number == -1) return {
-			"japanese": "何" + counter.word + "の" + example.word,
-			"furigana": counter.howMany + "の" + example.furi,
-			"translation": "how many " + example.translation_en[1] + "?",
-			"image": "counter-images/" + example.image
-		};
-		return {
-			"japanese": kanjiNumber(number) + counter.word + "の" + example.word,
-			"furigana": kanaNumber(counter, number) + "の" + example.furi,
-			"translation": number + " " + example.translation_en[number == 1 ? 0 : 1],
-			"image": "counter-images/" + example.image
-		};
-	};
-
-	const createSelfCounterFlashcard = (counter, number) => {
-		if (number == -1) return {
-			"japanese": "何" + counter.word,
-			"furigana": counter.howMany,
-			"translation": "how many " + counter.translation_en[1] + "?",
-			"image": null
-		};
-		return {
-			"japanese": kanjiNumber(number) + counter.word,
-			"furigana": kanaNumber(counter, number),
-			"translation": number + " " + counter.translation_en[number == 1 ? 0 : 1],
-			"image": null
-		};
-	};
-
 	const createRandomFlashcard = () => {
+
+		const kanjiNumber = (number) => {
+			const pos0 = number % 10;
+			const pos1 = Math.floor(number / 10);
+			const kanji = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+			const kanjiTen = "十";
+			if (pos1 == 0) return kanji[pos0];
+			if (pos1 == 1) return kanjiTen + kanji[pos0];
+			return kanji[pos1] + kanjiTen + kanji[pos0];
+		}
+
+		const kanaNumber = (counter, number) => {
+			if (counter.exceptions) {
+				const exc = counter.exceptions.find(e => e.number === number);
+				if (exc) return exc.reading;
+			}
+			const pos0 = number % 10;
+			const pos1 = Math.floor(number / 10);
+			const kana = ["", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"];
+			const kanaTen = "じゅう";
+			let conj = counter.conjunctions;
+			if (conj === "default") conj = ["じゅう", "いち", "に", "さん", "よん", "ご", "ろく", "なな", "はち", "きゅう"]
+				.map(s => s + counter.furi);
+
+			if (pos1 == 0) return conj[pos0]; // 1-9
+			if (number == 10) return conj[0]; // 10
+			if (pos1 == 1) return kanaTen + conj[pos0]; // 11-19
+			if (pos0 == 0) return kana[pos1] + conj[0]; // 20, 30,.. 90
+			return kana[pos1] + kanaTen + conj[pos0]; // 21, 22,..
+		}
+
+		const randomFlashcardCount = () => {
+			const rand = Math.random();
+			if (rand < 0.36) return Math.ceil(rand / 0.36 * 10); // 1 to 10
+			if (rand < 0.63) return Math.ceil((rand - 0.36) / 0.27 * 10 + 10); // 11 to 20
+			if (rand < 0.765) return Math.floor((rand - 0.63) / 0.135 * 7 + 3) * 10; // 30, 40... to 90
+			if (rand < 0.9) return Math.floor((rand - 0.765) / 0.135 * 79 + 21); // 21, 22... to 99
+			return -1; // interrogative (e.g. nankai)
+		}
+
+		const createDateFlashcard = () => {
+			let month = Math.floor(Math.random() * 12 + 1); // 1 = Jan
+			let maxDays;
+			switch (month) {
+				case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+					maxDays = 31; break;
+				case 4: case 6: case 9: case 11:
+					maxDays = 30; break;
+				default: maxDays = 28; break;
+			}
+			let day = Math.ceil(Math.random() * maxDays);
+			if (Math.random() < 0.00069) { // lucky enough for Feb 29?
+				month = 2;
+				day = 29;
+			}
+			let suffixEn;
+			switch (day) {
+				case 1: case 21: case 31: suffixEn = "st"; break;
+				case 2: case 22: suffixEn = "nd"; break;
+				default: suffixEn = "th"; break;
+			}
+
+			const monthNamesJa = ["いちがつ", "にがつ", "さんがつ", "しがつ", "ごがつ", "ろくがつ",
+				"しちがつ", "はちがつ", "くがつ", "じゅうがつ", "じゅういちがつ", "じゅうにがつ"];
+			const monthNamesEn = ["January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December"];
+			const dayReadings = ["ついたち", "ふつか", "みっか", "よっか", "いつか", "むいか",
+				"なのか", "ようか", "ここのか", "とおか", "じゅういちにち", "じゅうににち", "じゅうさんにち",
+				"じゅうよっか", "じゅうごにち", "じゅうろくにち", "じゅうななにち", "じゅうはちにち",
+				"じゅうくにち", "はつか", "にじゅういちにち", "にじゅうににち", "にじゅうさんにち",
+				"にじゅうよっか", "にじゅうごにち", "にじゅうろくにち", "にじゅうななにち",
+				"にじゅうはちにち", "にじゅうくにち", "さんじゅうにち", "さんじゅういちにち"];
+
+			return {
+				"japanese": kanjiNumber(month) + "月" + kanjiNumber(day) + "日",
+				"furigana": monthNamesJa[month - 1] + dayReadings[day - 1],
+				"translation": day + suffixEn + " of " + monthNamesEn[month - 1],
+				"image": null
+			}
+		};
+
+		const createCounterGroupFlashcard = (counter, number) => {
+			const example = counter.examples[Math.floor(Math.random() * counter.examples.length)];
+			if (number == -1) return {
+				"japanese": "何" + counter.word + "の" + example.word,
+				"furigana": counter.howMany + "の" + example.furi,
+				"translation": "how many " + example.translation_en[1] + "?",
+				"image": "counter-images/" + example.image
+			};
+			return {
+				"japanese": kanjiNumber(number) + counter.word + "の" + example.word,
+				"furigana": kanaNumber(counter, number) + "の" + example.furi,
+				"translation": number + " " + example.translation_en[number == 1 ? 0 : 1],
+				"image": "counter-images/" + example.image
+			};
+		};
+
+		const createSelfCounterFlashcard = (counter, number) => {
+			if (number == -1) return {
+				"japanese": "何" + counter.word,
+				"furigana": counter.howMany,
+				"translation": "how many " + counter.translation_en[1] + "?",
+				"image": null
+			};
+			return {
+				"japanese": kanjiNumber(number) + counter.word,
+				"furigana": kanaNumber(counter, number),
+				"translation": number + " " + counter.translation_en[number == 1 ? 0 : 1],
+				"image": null
+			};
+		};
+
 		// +1 counter is the special case for month & day
 		const countersTotal = counters.counterGroups.length + counters.selfCounters.length + 1;
 		let drawIndex = Math.floor(Math.random() * countersTotal) - 1;
@@ -124,38 +125,83 @@
 		return createSelfCounterFlashcard(counters.selfCounters[drawIndex], randomCount);
 	};
 
-	const flashcardDom = {
+	const htmlRefs = {
 		flashcard: document.querySelector(".flashcard"),
 		jp: document.querySelector(".flashcard .japanese rb"),
 		jpFuri: document.querySelector(".flashcard .japanese rt"),
 		translation: document.querySelector(".flashcard .translation"),
-		image: document.querySelector(".flashcard .card-image img")
+		image: document.querySelector(".flashcard .card-image img"),
+		speedDisplayCurrent: document.querySelector(".speed-display .current"),
+		speedDisplayFadingOut: document.querySelector(".speed-display .fading-out")
 	}
 
 	let isAnswerShown = true;
 	let currentCard;
 
+	let timerStart = -1;
+	let continueClickCount = 0;
+	let continueClickedLastOn = -1;
+
 	const assignNewFlashcard = () => {
 		currentCard = createRandomFlashcard();
-		flashcardDom.flashcard.classList.add("hide-answer");
-		flashcardDom.jp.innerHTML = "　";
-		flashcardDom.jpFuri.innerHTML = "　";
-		flashcardDom.translation.innerHTML = currentCard.translation;
-		flashcardDom.image.style.visibility = currentCard.image === null ? "hidden" : "visible";
-		flashcardDom.image.src = currentCard.image === null ? "" : currentCard.image;
+		htmlRefs.flashcard.classList.add("hide-answer");
+		htmlRefs.jp.innerHTML = "　";
+		htmlRefs.jpFuri.innerHTML = "　";
+		htmlRefs.translation.innerHTML = currentCard.translation;
+		htmlRefs.image.style.visibility = currentCard.image === null ? "hidden" : "visible";
+		htmlRefs.image.src = currentCard.image === null ? "" : currentCard.image;
 	}
 
 	const showAnswer = () => {
-		flashcardDom.flashcard.classList.remove("hide-answer");
-		flashcardDom.jp.innerHTML = currentCard.japanese;
-		flashcardDom.jpFuri.innerHTML = currentCard.furigana;
+		htmlRefs.flashcard.classList.remove("hide-answer");
+		htmlRefs.jp.innerHTML = currentCard.japanese;
+		htmlRefs.jpFuri.innerHTML = currentCard.furigana;
 	}
 
 	const onContinueClicked = () => {
 		if (isAnswerShown) assignNewFlashcard();
 		else showAnswer();
 		isAnswerShown = !isAnswerShown;
+
+		const time = document.timeline.currentTime;
+		const sinceLast = time - continueClickedLastOn;
+		if (timerStart === -1) {
+			timerStart = time;
+			window.setInterval(updateCounter, 30_000);
+		}
+		if (sinceLast > 300 && sinceLast < 40_000) { // don't count spam or long delays
+			continueClickCount++;
+		}
+		continueClickedLastOn = time;
 	};
+
+	const updateCounter = () => {
+		if (continueClickCount < 3 || timerStart === -1) return;
+		const elapsedMs = document.timeline.currentTime - timerStart;
+		const elapsedMinutes = elapsedMs / 60_000;
+		const answersTotal = continueClickCount / 2;
+		const secondsPerCard = elapsedMs / 1000 / answersTotal;
+
+		const message = Math.floor(answersTotal) + " cards in " + elapsedMinutes.toFixed(1) + " min ("
+			+ secondsPerCard.toFixed() + "s per answer)";
+		const cur = htmlRefs.speedDisplayCurrent;
+		const last = htmlRefs.speedDisplayFadingOut;
+
+		last.textContent = cur.textContent;
+		cur.textContent = message;
+		requestAnimationFrame(() => {
+			cur.style.opacity = "0";
+			last.style.opacity = "1";
+			cur.style.transition = "none";
+			last.style.transition = "none";
+			requestAnimationFrame(() => {
+				cur.style.transition = "opacity 1.5s ease-in-out";
+				last.style.transition = "opacity 1.5s ease-in-out";
+				cur.style.opacity = "1";
+				last.style.opacity = "0";
+			});
+		});
+	}
 
 	document.querySelector(".continue-button").onclick = e => onContinueClicked();
 	document.body.addEventListener('keypress', e => { if (e.code === "Space") onContinueClicked() });
